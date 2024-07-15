2328737adcb4a3f55d6e934b6d93dfd907961af6
Andres Osorio <anosoriom@unal.edu.co>
2024-07-15 12:44:38 -0500

    [Win11Acer\main\04]: Se agrega cambios a clasesExm.py para incluir division entera y residuo de dos numeros

--- clasesExm.py
+++ clasesExm.py
@@ -2,7 +2,8 @@
 def suma(a,b):
     return a+b
 
-# Funcion para restar dos numeros
+
+# Comentario actualizado: Funcion para calcular la resta dos nums 
 def resta(a,b):
     return b-a
 
@@ -10,7 +11,14 @@ def resta(a,b):
 def multiplicar(a,b):
     return a*b
 
-# Funcion para multiplicar dos numeros
+# Funcion para dividir dos numeros
 def dividir(a,b):
     return b/a
 
+# Funcion para dividision entera dos numeros
+def divEntera(a,b):
+   return b//a
+
+# Funcion para residuo de la division de dos numeros
+def divResiduo(a,b):
+    return b%a
