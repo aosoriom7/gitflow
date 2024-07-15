1fe5bb6caadc9f22d9fe5a1a3697d7b93706f1b2
Andres Osorio <anosoriom@unal.edu.co>
2024-07-15 11:31:21 -0500

    [Win11Acer\feat01\02]: Atualizacion operaciones en clasesExm.py y creacion de archivo opeacionestodas.ipynb

--- clasesExm.py
+++ clasesExm.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Funcion para sumar dos numeros
 def suma(a,b):
     return a+b
@@ -9,3 +11,15 @@ def resta(a,b):
 # Funcion para multiplicar dos numeros
 def multiplicar(a,b):
     return a*b
+
+# Funcion para calcular el cuadrado de un numero
+def cuadrado(a):
+    return a**2
+
+ # Funcion para calcular la raiz de un numero
+def raizcuad(a):
+    return np.sqrt(a)
+
+ # Funcion para calcular el logaritmo natural de un numero
+def logaritmo(a):
+    return np.log(a)
